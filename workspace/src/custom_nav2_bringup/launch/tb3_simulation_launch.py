524632d218b049f9df1a96061524b22a31ea1ac0
Remma Matsuda <titech.renma@gmail.com>
2025-02-25 20:47:21 +0900

    change shared directory.

--- workspace/src/custom_nav2_bringup/launch/tb3_simulation_launch.py
+++ workspace/src/custom_nav2_bringup/launch/tb3_simulation_launch.py
@@ -28,7 +28,7 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     # Get the launch directory
-    bringup_dir = get_package_share_directory('nav2_bringup')
+    bringup_dir = get_package_share_directory('custom_nav2_bringup')
     launch_dir = os.path.join(bringup_dir, 'launch')
 
     # Create the launch configuration variables
